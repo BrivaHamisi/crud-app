5a75dca79a31a19100071605c5e96b8b1a925699
BrivaHamisi <brivahamisi@gmail.com>
2024-08-21 16:06:00 +0300

    Nest Js Crud with Myql

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto) {
+    return this.todosService.create(createTodoDto);
+  }
+
+  @Get()
+  async findAll():Promise<Todo[]> {
+    return await this.todosService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number):Promise<Todo> {
+    const  result = await this.todosService.findOne(id)
+  
+    return result;
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: number, @Body() updateTodoDto: UpdateTodoDto):Promise<Todo> {
+     return await this.todosService.update(id, updateTodoDto)
+  }
+
+
+  @Delete('delete/:id')
+  remove(@Param('id') id: number) {
+    return this.todosService.remove(+id);
+  }
+}
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TodosService {
+  constructor(@InjectRepository(Todo) private readonly todoRepository:Repository<Todo>) {};
+
+  async create(createTodoDto:CreateTodoDto) {
+    const todo = this.todoRepository.create(createTodoDto);
+    return await this.todoRepository.save(todo);
+  }
+
+  async findAll():Promise<Todo[]> {
+    return await this.todoRepository.find();
+  }
+
+  async findOne(id: number):Promise<Todo> {
+    return await this.todoRepository.findOneBy({id});
+  }
+
+  // Update not working yet
+
+  async update(id: number, updateTodoDto: UpdateTodoDto):Promise<Todo> {
+
+    const todo = await this.todoRepository.preload({id, ...updateTodoDto});
+
+    console.log('Preloaded Todo:', todo);
+
+    if (!todo) {
+      throw new NotFoundException(`Todo with id ${id} not found.`);
+    }
+    
+    return this.todoRepository.save(todo);
+  }
+
+  remove(id: number) {
+    return this.todoRepository.delete(id);
+  }
+}
